bc231aacffe19d41eff0dc44784a49b29a008718
Luca Janssens <lucajanssens@gmail.com>
2021-06-12 17:18:30 +0200

    Adding features.

--- src/components/Mindnode.jsx
+++ src/components/Mindnode.jsx
@@ -1,13 +1,27 @@
 import React, {Fragment} from 'react'
-import ReactFlow, {removeElements, addEdge, Background, Controls, MiniMap} from 'react-flow-renderer'
+import ReactFlow, {removeElements, addEdge, Background, Controls, MiniMap, updateEdge} from 'react-flow-renderer'
+import CustomEdge from "./CustomEdge"
 
 const initialElements = [ {
-    id: '1', type: 'input', data:{label: "Idea"}, position: {x:0, y:0}
-}]
+    id: '1', type: 'input', data:{label: "Main idea"}, position: {x:0, y:0}},
+    {
+        id: '3',
+        data: { label: 'Related information' },
+        position: { x: 400, y: 200 },
+      },
+      { id: 'e1-2', source: '1', target: '3', label: 'Update me' },
+]
+
+const edgeTypes = {
+    custom: CustomEdge,
+  };
 
 function onLoad(reactFlowInstance) {
     reactFlowInstance.fitView();
 }
+
+function onNodeDragStop(event, node){ console.log('drag stop', node)};
+function onElementClick(event, element){ console.log('click', element)};
 
 function Mindnode() {
     const [elements, setElements] = React.useState(initialElements);
@@ -37,6 +51,10 @@ function Mindnode() {
             snapGrid={[16,16]} 
             snapToGrid={true} 
             onElementsRemove={onElementsRemove}
+            onElementClick={onElementClick}
+            onNodeDragStop={onNodeDragStop}
+            edgeTypes={edgeTypes}
+            key="edges"
             style={{width:'100%', height:'90vh'}}>
                 <Background color="#888" gap={16} />
                 <MiniMap nodeColor={ n => {
@@ -44,8 +62,9 @@ function Mindnode() {
                     return "#FFCC00" 
                     }} />
                 <Controls />
+                
             </ReactFlow>
-            <div> 
+            <div className="idea-maker"> 
                 <input type="text" name="title" onChange={ e => setName(e.target.value)} />
                 <button type="button" onClick={addNode}>Add to the idea flow</button>
             </div>
